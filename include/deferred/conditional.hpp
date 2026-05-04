163289165dc4e9737b6d3502b06c98977753bbc9
Yiannis Papadopoulos <giannis.papadopoulos@gmail.com>
2026-05-04 17:55:08 -0400

    feat: implement if_then deferred expression with optional result type

--- include/deferred/conditional.hpp
+++ include/deferred/conditional.hpp
@@ -4,6 +4,7 @@
 #ifndef DEFERRED_CONDITIONAL_HPP
 #define DEFERRED_CONDITIONAL_HPP
 
+#include <optional>
 #include <tuple>
 #include <type_traits>
 #include <utility>
@@ -12,6 +13,105 @@
 #include "expression.hpp"
 
 namespace deferred {
+
+/**
+ * @brief Deferred conditional that evaluates @p ThenExpression if
+ * @p ConditionExpression evaluates to @c true.
+ *
+ * @tparam ConditionExpression Type of the condition expression.
+ * @tparam ThenExpression Type of the expression to evaluate if the condition is @c true.
+ */
+template<Deferred ConditionExpression, Deferred ThenExpression>
+class if_then_expression
+{
+public:
+  using condition_expression_type = ConditionExpression;
+  using then_expression_type      = ThenExpression;
+  using then_result_type          = decltype(std::declval<ThenExpression>()());
+  using result_type =
+    std::conditional_t<std::is_void_v<then_result_type>, void, std::optional<then_result_type>>;
+  using subexpression_types = std::tuple<ConditionExpression, ThenExpression>;
+
+private:
+  [[no_unique_address]] condition_expression_type m_condition;
+  [[no_unique_address]] then_expression_type m_then;
+
+public:
+  /**
+   * @brief Constructs an if_then_expression.
+   * @tparam Condition Type of the condition expression.
+   * @tparam ThenEx Type of the then expression.
+   * @param condition Condition expression.
+   * @param then_ Then expression.
+   */
+  template<typename Condition, typename ThenEx>
+  constexpr explicit if_then_expression(Condition&& condition, ThenEx&& then_) :
+    m_condition(std::forward<Condition>(condition)), m_then(std::forward<ThenEx>(then_))
+  { }
+
+  /**
+   * @brief Evaluates the conditional expression.
+   * @return Result of the conditional expression.
+   */
+  [[nodiscard]] constexpr result_type operator()() const
+  {
+    if (evaluate(m_condition))
+    {
+      if constexpr (std::is_void_v<then_result_type>)
+      {
+        evaluate(m_then);
+      }
+      else
+      {
+        return std::optional<then_result_type>{evaluate(m_then)};
+      }
+    }
+    else
+    {
+      if constexpr (!std::is_void_v<then_result_type>)
+      {
+        return std::nullopt;
+      }
+    }
+  }
+
+  /// @copydoc if_then_expression::operator()() const
+  [[nodiscard]] constexpr result_type operator()()
+  {
+    if (evaluate(m_condition))
+    {
+      if constexpr (std::is_void_v<then_result_type>)
+      {
+        evaluate(m_then);
+      }
+      else
+      {
+        return std::optional<then_result_type>{evaluate(m_then)};
+      }
+    }
+    else
+    {
+      if constexpr (!std::is_void_v<then_result_type>)
+      {
+        return std::nullopt;
+      }
+    }
+  }
+
+  /**
+   * @brief Visits the expression with a visitor.
+   * @tparam Visitor Type of the visitor.
+   * @param v Visitor.
+   * @param nesting Nesting level.
+   */
+  template<typename Visitor>
+  constexpr void visit(Visitor&& v, std::size_t nesting = 0) const
+  {
+    std::forward<Visitor>(v)(*this, nesting);
+    std::forward<Visitor>(v)(m_condition, nesting + 1);
+    std::forward<Visitor>(v)(m_then, nesting + 1);
+  }
+};
 
 /**
  * @brief Deferred conditional that evaluates @p ThenExpression if
@@ -127,6 +227,29 @@ if_then_else(ConditionExpression&& condition, ThenExpression&& then_, ElseExpres
     std::forward<ConditionExpression>(condition),
     std::forward<ThenExpression>(then_),
     std::forward<ElseExpression>(else_));
+}
+
+/**
+ * @brief Creates a @c deferred conditional that evaluates @p then_ if @p condition
+ * evaluates to @c true.
+ *
+ * The result type of <tt>if_then_expression(...)()</tt> is @c std::optional<T> if
+ * @p then_ returns @c T (where @c T is not @c void), otherwise it is @c void.
+ *
+ * @tparam ConditionExpression Type of the condition expression.
+ * @tparam ThenExpression Type of the then expression.
+ * @param condition Condition expression.
+ * @param then_ Then expression.
+ * @return An @ref if_then_expression that will perform the deferred evaluation.
+ */
+template<typename ConditionExpression, typename ThenExpression>
+[[nodiscard]] constexpr auto if_then(ConditionExpression&& condition, ThenExpression&& then_)
+{
+  using condition_expression = make_deferred_t<ConditionExpression>;
+  using then_expression      = make_deferred_t<ThenExpression>;
+  return if_then_expression<condition_expression, then_expression>(
+    std::forward<ConditionExpression>(condition),
+    std::forward<ThenExpression>(then_));
 }
 
 } // namespace deferred
--- test/unit/conditional.cpp
+++ test/unit/conditional.cpp
@@ -67,3 +67,48 @@ TEST_CASE("conditional with mutable lambda", "[conditional-mutable-lambdas]")
   CHECK(j == 0);
   CHECK(k == 0);
 }
+
+TEST_CASE("if_then with literal", "[if_then-literal]")
+{
+  auto ex_true  = deferred::if_then(true, 42);
+  auto res_true = ex_true();
+  static_assert(std::is_same_v<decltype(res_true), std::optional<int>>);
+  CHECK(res_true.has_value());
+  CHECK(*res_true == 42);
+
+  auto ex_false  = deferred::if_then(false, 42);
+  auto res_false = ex_false();
+  CHECK(!res_false.has_value());
+}
+
+TEST_CASE("if_then with void return", "[if_then-void]")
+{
+  int i   = 0;
+  auto ex = deferred::if_then(true, [&] { ++i; });
+  static_assert(std::is_void_v<decltype(ex())>);
+  ex();
+  CHECK(i == 1);
+
+  auto ex2 = deferred::if_then(false, [&] { ++i; });
+  ex2();
+  CHECK(i == 1);
+}
+
+TEST_CASE("if_then with constexpr", "[if_then-constexpr]")
+{
+  constexpr auto ex = deferred::if_then(true, 42);
+  static_assert(ex().has_value());
+  static_assert(*ex() == 42);
+
+  constexpr auto ex_false = deferred::if_then(false, 42);
+  static_assert(!ex_false().has_value());
+}
+
+TEST_CASE("if_then visit", "[if_then-visit]")
+{
+  auto ex   = deferred::if_then(true, 42);
+  int count = 0;
+  ex.visit([&](auto const&, std::size_t) { ++count; });
+  // 1 (if_then_expression) + 1 (condition) + 1 (then) = 3
+  CHECK(count == 3);
+}
